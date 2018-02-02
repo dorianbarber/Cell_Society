0ff290e59abdb96a496dfb3783a6a9e8c8d86f45
Conrad Mitchell <cmm134@duke.edu>
2018-02-02 03:16:19 -0500

    added very basic implementation to model class and GoL sim.

--- src/cellsociety_team09/Model.java
+++ src/cellsociety_team09/Model.java
@@ -1,5 +1,14 @@
 package cellsociety_team09;
 
-public class Model {
+public abstract class Model {
+	
+		
+	public abstract int updateCell(int[] state, int[]neighborstates); 
+	
+	public abstract Cell createCell(int state[]); 
+	
+	
+	
+	
 
 }
