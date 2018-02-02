991b45a88d707e9b550f3d684fde04a706f8726b
Dorian Barber <dpb20@duke.edu>
2018-02-02 16:46:46 -0500

    Add field comments

--- src/cellsociety_team09/Cell.java
+++ src/cellsociety_team09/Cell.java
@@ -3,20 +3,25 @@ package cellsociety_team09;
 import javafx.scene.shape.Shape;
 
 public abstract class Cell {
+	//the shape of the cell in the simulation
 	private Shape cellShape;
 	
 	private int currentState;
 	private int previousState;
 	private int[] possibleStates;
 	
+	//requires an instance of Model to update the cell class
+	private Model simulation;
+	
 	private int[] neighborStates;
 	
-	public Cell (int[] n) {
+	public Cell (int[] n, Model sim) {
+		simulation = sim;
 		possibleStates = n;
 	}
 	
 	public void update() {
 		previousState = currentState;
-		currentState = Model.updateCell(possibleStates, neighborStates);
+		currentState = simulation.updateCell(possibleStates, neighborStates);
 	}
 }
