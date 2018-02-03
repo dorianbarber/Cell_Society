99d66cd6fb0bc6cad0479d84e1626ce71a25fd1a
Dorian Barber <dpb20@duke.edu>
2018-02-03 12:46:09 -0500

    Add cell information gathering in Grid class

--- src/cellsociety_team09/Grid.java
+++ src/cellsociety_team09/Grid.java
@@ -1,15 +1,52 @@
 package cellsociety_team09;
 
+import javafx.scene.Group;
+
 public class Grid {
-	private Cell[][] gridCells;
+	private CellModel[][] gridCells;
 	private int gridSize;
-	private Model simulation;
+	private Group cellSet;
+	private static final CellModel[] possibleModels= {
+			new Life_Model()
+	};
 	
 	public Grid(int n) {
 		gridSize = n;
-		gridCells = new Cell[gridSize][gridSize];
+		gridCells = new CellModel[gridSize][gridSize];
+		for(int i = 0; i < gridSize; i++) {
+			for(int j = 0; j < gridSize; j++) {
+				gridCells[i][j] = new CellModel();
+			}
+		}
+	}
+	
+	
+	public Group getCells() {
 		
-		
-		
+	}
+	
+	/**
+	 * Loops through the cells to let them find their
+	 * neighbors and also get their next state
+	 */
+	public void findCellNeighbors() {
+		for(int i = 0; i < gridSize; i++) {
+			for(int j = 0; j < gridSize; j++) {
+				gridCells[i][j].getNeighbors();
+				gridCells[i][j].findNextState();
+			}
+		}
+	}
+	
+	/**
+	 * Loops through cells to move each one to their next state
+	 */
+	public void moveSimulationForward() {
+		for(int i = 0; i < gridSize; i++) {
+			for(int j = 0; j < gridSize; j++) {
+				gridCells[i][j].moveForward();
+			}
+		}
+		this.findCellNeighbors();
 	}
 }
