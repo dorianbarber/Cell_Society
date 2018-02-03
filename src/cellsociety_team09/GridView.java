971478054dc5f2d1fc356151b26d855d6b711be7
Liam <lp139@duke.edu>
2018-02-03 18:02:02 -0500

    Add random grid patterns and all buttons except for backstep

--- src/cellsociety_team09/GridView.java
+++ src/cellsociety_team09/GridView.java
@@ -1,5 +1,7 @@
 package cellsociety_team09;
 
+import java.util.Random;
+
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -22,6 +24,29 @@ public class GridView {
 			for (double j = gridYPosition; j < gridYPosition + gridSize; j += gridBlockSize){
 				Rectangle toAdd = new Rectangle(i, j, blocksize, blocksize);
 				toAdd.setFill(Color.ANTIQUEWHITE);
+				toAdd.setStroke(Color.BLACK);
+				retgroup.getChildren().add(toAdd);
+			}
+		}
+		return retgroup;
+	}
+	public Group drawRandomGrid(int screenwidth, int screenheight, int blocksize){
+		Group retgroup = new Group();
+		for (double i = gridXPosition; i < gridXPosition + gridSize; i += gridBlockSize){
+			for (double j = gridYPosition; j < gridYPosition + gridSize; j += gridBlockSize){
+				Rectangle toAdd = new Rectangle(i, j, blocksize, blocksize);
+				if (new Random().nextInt(3) == 0){
+					toAdd.setFill(Color.BLUE);
+				}
+				else if (new Random().nextInt(3) == 1){
+					toAdd.setFill(Color.RED);
+				}
+				else if (new Random().nextInt(3) == 2){
+					toAdd.setFill(Color.GREEN);
+				}
+				else{
+					toAdd.setFill(Color.YELLOWGREEN);
+				}
 				toAdd.setStroke(Color.BLACK);
 				retgroup.getChildren().add(toAdd);
 			}
--- src/cellsociety_team09/Menu.java
+++ src/cellsociety_team09/Menu.java
@@ -1,9 +1,16 @@
     package cellsociety_team09;
 
+import java.util.Random;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -14,6 +21,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Menu extends Application{
 	
@@ -21,17 +29,31 @@ public class Menu extends Application{
 	private Scene myScene;
 	private Group myRoot;
 	
+	private Rectangle test = new Rectangle(30,30);;
+	
+	public static final int FRAMES_PER_SECOND = 1;
+	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+
+	//public static final double SECOND_DELAY = 6.0 / FRAMES_PER_SECOND;
+	private Timeline animation;
+	
 	private final int WIDTH = 700;
 	private final int HEIGHT = 600;
 	private final int GRIDSIZE = 500;
 	private final int GRIDX = WIDTH / 20; 
 	private final int GRIDY = HEIGHT / 20;
 	private final Color BACKGROUND = Color.ANTIQUEWHITE;
-	private final int SLIDEROFFSET = 10;
-	private final double BUTTONOFFSET = 2.5 * SLIDEROFFSET;
+	private final int SLIDEROFFSET = 6;
+	private final double BUTTONVERTOFFSET = 25;
+	private final double BUTTONHOROFFSET = 45;
 	private final int DROPOFFSET = 15;
+	private final int SLIDERSIZE = 300;
+	private final int BUTTONSIZE = 30;
 	private GridView myGrid;
 	private int blocksize;
+	private double stepincrement = FRAMES_PER_SECOND;
+	private double sliderx;
+	private Group gridgroup;
 	
 	
     /**
@@ -47,6 +69,31 @@ public class Menu extends Application{
 		myStage = stage;
 		myStage.setScene(myScene);
 		myStage.show();
+		
+		animate();
+	}
+	public void animate(){
+		// attach "game loop" to timeline to play it
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(stepincrement));
+		animation = new Timeline();
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+	}
+	private void step(double elapsedTime) {
+//		myRoot.getChildren().remove(test);
+//		Random rand = new Random();
+//		if (test.getFill() == Color.BLUE){	
+//			test.setFill(Color.RED);
+//		}
+//		else {
+//			test.setFill(Color.BLUE);
+//		}
+//		myRoot.getChildren().add(test);
+		//System.out.println(stepincrement);
+		//System.out.println("HI");
+		myRoot.getChildren().remove(gridgroup);
+		gridgroup = myGrid.drawRandomGrid(WIDTH, HEIGHT, blocksize);
+		myRoot.getChildren().add(gridgroup);
 	}
 	
 	private Scene initializeStart(int screenwidth, int screenheight, Color paint){
@@ -55,37 +102,83 @@ public class Menu extends Application{
 		myRoot = root;
 		myGrid = new GridView(GRIDX, GRIDY, blocksize, GRIDSIZE);
 		Scene scene = new Scene(root, screenwidth, screenheight, paint);
-		root.getChildren().add(myGrid.drawBlankGrid(screenwidth, screenheight, blocksize));
+		gridgroup = myGrid.drawBlankGrid(screenwidth, screenheight, blocksize);
+		root.getChildren().add(gridgroup);
 		root.getChildren().add(getAnimationSpeedSlider());
 		root.getChildren().add(getPlayButton());
 		root.getChildren().add(getPauseButton());
+		root.getChildren().add(getStepForwardButton());
 		root.getChildren().add(getMenu());
 		root.getChildren().add(getText());
+		root.getChildren().add(getBackStepButton());
 		
 		return scene;
 	}
 	private Slider getAnimationSpeedSlider(){
 		Slider speedtoggle = new Slider();
 		speedtoggle.setLayoutY(myGrid.getY() + myGrid.getDimensions() + SLIDEROFFSET); 
-		speedtoggle.setMinWidth(300);
-		speedtoggle.setMaxWidth(300);
+		speedtoggle.setMinWidth(SLIDERSIZE);
+		speedtoggle.setMaxWidth(SLIDERSIZE);
+		speedtoggle.setMin(1);
+		speedtoggle.setMax(10);
 		speedtoggle.setLayoutX(myGrid.getX() + myGrid.getDimensions() / 2 - speedtoggle.getMaxWidth() / 2);
+		sliderx = speedtoggle.getLayoutX();
+		speedtoggle.valueProperty().addListener(new ChangeListener<Number>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Number> observable,
+		            Number oldValue, Number newValue) {
+		    	stepincrement = (double) newValue;
+		    	animation.setRate(stepincrement * FRAMES_PER_SECOND);
+		    }
+		});
 		return speedtoggle;
 	}
+
+	private Button getBackStepButton(){
+		
+		Image play = new Image(getClass().getResourceAsStream("../pauseicon.png"), 30, 30, false, false);
+		Button pausebutton = new Button("", new ImageView(play));
+		pausebutton.setLayoutX(sliderx + 13.5);
+		pausebutton.setLayoutY(myGrid.getY() + myGrid.getDimensions() + BUTTONVERTOFFSET);
+		pausebutton.setOnAction(e -> getPauseAction());
+		return pausebutton;
+	}
+//	private void getBackStep() {
+//		animation.pause();
+//		animation.setRate(-1 * animation.getRate());
+//		animation.`	
+//		animation.setRate(-1 * animation.getRate());
+//	}
+
 	private Button getPlayButton(){
-		Image play = new Image(getClass().getResourceAsStream("../playicon.png"), 30, 30, false, false);
+		Image play = new Image(getClass().getResourceAsStream("../playicon.png"), BUTTONSIZE, BUTTONSIZE, false, false);
 		Button playbutton = new Button("", new ImageView(play));
-		playbutton.setLayoutX(myGrid.getX() + myGrid.getDimensions() / 2 - 3 * BUTTONOFFSET);
-		playbutton.setLayoutY(myGrid.getY() + myGrid.getDimensions() + BUTTONOFFSET);
+		playbutton.setLayoutX(sliderx + BUTTONHOROFFSET + BUTTONSIZE + 13.5);
+		playbutton.setLayoutY(myGrid.getY() + myGrid.getDimensions() + BUTTONVERTOFFSET);
+		playbutton.setOnAction(e -> getPlayAction());
 		return playbutton;
 	}
 	private Button getPauseButton(){
 		
 		Image play = new Image(getClass().getResourceAsStream("../pauseicon.png"), 30, 30, false, false);
-		Button playbutton = new Button("", new ImageView(play));
-		playbutton.setLayoutX(myGrid.getX() + myGrid.getDimensions() / 2 + BUTTONOFFSET);
-		playbutton.setLayoutY(myGrid.getY() + myGrid.getDimensions() + BUTTONOFFSET);
-		return playbutton;
+		Button pausebutton = new Button("", new ImageView(play));
+		pausebutton.setLayoutX(sliderx + 2 * BUTTONHOROFFSET + 2 * BUTTONSIZE + 13.5);
+		pausebutton.setLayoutY(myGrid.getY() + myGrid.getDimensions() + BUTTONVERTOFFSET);
+		pausebutton.setOnAction(e -> getPauseAction());
+		return pausebutton;
+	}
+	private Button getStepForwardButton() {
+		Image play = new Image(getClass().getResourceAsStream("../playicon.png"), BUTTONSIZE, BUTTONSIZE, false, false);
+		Button stepforwardbutton = new Button("", new ImageView(play));
+		stepforwardbutton.setLayoutX(sliderx + 3 * BUTTONHOROFFSET + 3 * BUTTONSIZE + 13.5);
+		stepforwardbutton.setLayoutY(myGrid.getY() + myGrid.getDimensions() + BUTTONVERTOFFSET);
+		stepforwardbutton.setOnAction(e -> getStepForward());
+		return stepforwardbutton;
+	}
+
+	private void getStepForward() {
+		animation.pause();
+		step(stepincrement);
 	}
 	private ComboBox<String> getMenu(){
 		ObservableList<String> options = 
@@ -117,6 +210,12 @@ public class Menu extends Application{
 		retroot.getChildren().add(background);
 		
 		return retroot;
+	}
+	private void getPlayAction(){
+		animation.play();
+	}
+	private void getPauseAction(){
+		animation.pause();
 	}
 }
 
