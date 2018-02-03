c00c75d7d36722b7245281314cb7b39eebfee0ed
Dorian Barber <dpb20@duke.edu>
2018-02-02 20:58:42 -0500

    Edit StateNode

--- src/cellsociety_team09/CellModel.java
+++ src/cellsociety_team09/CellModel.java
@@ -1,29 +1,26 @@
 package cellsociety_team09;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
 
 public abstract class CellModel {
 	private Shape shape;
-	private stateNode state;
+	private Color color;
+	private StateNode state;
 	private CellModel[] neighbors;
 	
 	
+	public CellModel() {
+		
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	abstract void getNeighbors();
 	
 	abstract void findNextState();
+
+	public void moveForward() {
+		state.moveForward();
+	}
+	
+	
 }
