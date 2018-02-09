e6f8fe730e217ceccd2e6ea8e55de3a26ed2ef76
Conrad Mitchell <cmm134@duke.edu>
2018-02-09 15:23:43 -0500

    add new cell and grid for the abstraction

--- src/simulations/RPSCell.java
+++ src/simulations/RPSCell.java
@@ -1,5 +1,6 @@
 package simulations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cellsociety_team09.StateNode;
@@ -8,55 +9,75 @@ import javafx.scene.paint.Color;
 public class RPSCell extends CellModel{
 
 	private static Color[] colors= {Color.WHITE, Color.BLUE, Color.RED, Color.GREEN};
+	private static int EMPTYCELL;
 	private static int ROCKCELL=1;
 	private static int PAPERCELL=2;
 	private static int SCISSORCELL=3;
-	
-	
+	private ArrayList<RPSCell> neighbors = new ArrayList<RPSCell>();
+	private int type;
+	private int power;
+	private RPSCell next;
 	
 	public RPSCell()
 	{
-		StateNode s = new StateNode(colors[0], new int[] {0});
-		state=s;
+		type=0;
+		power=10;
+	}
+	 
+	public RPSCell(int t, int p)
+	{
+		power=p;
+		type=t;
 	}
 	
-	
-	
-	
 	@Override
-	int[] getStates() {
-		// TODO Auto-generated method stub
-		return state.getStates();
+	public void addNeighbor(CellModel a)
+	{
+		neighbors.add((RPSCell)a);
 	}
-
-	@Override
-	public void findNextState() {
-		int cstate=getStates()[0]
-			if(cstate==ROCKCELL)
+	
+	public void nextState()
+	{
+		if(type==ROCKCELL)
+			for(int a=0; a<neighbors.size(); a++)
+				if(neighbors.get(a).getState()==SCISSORCELL)
+					neighbors.get(a).setNextState(type, 10);
 				
-		
-		
-		
-		// TODO Auto-generated method stub
-		
+		if(type==PAPERCELL)
+			for(int a=0; a<neighbors.size(); a++)
+				if(neighbors.get(a).getState()==ROCKCELL)
+					neighbors.get(a).setNextState(type, 10);
+			
+		if(type==SCISSORCELL)
+			for(int a=0; a<neighbors.size(); a++)
+				if(neighbors.get(a).getState()==PAPERCELL)
+					neighbors.get(a).setNextState(type, 10);
+			
+		for(int a=0; a<neighbors.size(); a++)
+			if(neighbors.get(a).getState()==EMPTYCELL && power>0)
+				neighbors.get(a).setNextState(type, power-1);
+	}
+	
+	
+	public int getState()
+	{
+		return type;
 	}
 
-	@Override
-	public void moveForward(List<List<CellModel>> cellgrid) {
-		// TODO Auto-generated method stub
-		
+	public RPSCell getNext()
+	{
+		return next;
 	}
-
 	@Override
 	public void getInput(List<Integer> states) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	protected void setNextState(StateNode a) {
-		// TODO Auto-generated method stub
-		
+	
+	protected void setNextState(int t, int p) {
+		next= new RPSCell(t,p);
 	}
+	
 
 }
--- src/simulations/RPSGrid.java
+++ src/simulations/RPSGrid.java
@@ -1,15 +1,59 @@
 package simulations;
 
+import com.sun.javafx.geom.Rectangle;
+
 public class RPSGrid extends GridModel{
 
+	private int size=0;
+	
 	
 	public RPSGrid()
 	{
-		for(int a=0; a<gridCells.get(0).size(); a++)
+		size=gridCells.get(0).size();
+		for(int a=0; a<size.size(); a++)
 		{
-			for(int b=0;)
+			for(int b=0; b<size.size(); b++)
+			{
+				RPSCell r = (RPSCell) gridCells.get(a).get(b);
+				gridCells.get(a).set(b,r);
+			}
 		}
+		NeighborFinder.getNeighbors(gridCells, new Rectangle(), 0);
 	}
+	
+	
+	@Override
+	public void update()
+	{
+		for(int r=0; r<size; r++)
+			for(int c=0; c<size; c++)
+				gridCells.get(r).get(c).nextState();
+	}
+	
+	@Override
+	public void moveForward()
+	{
+		for(int r=0; r<size; r++)
+			for(int c=0; c<size; c++)
+				gridCells.get(r).get(c)= gridCells.get(r).get(c).getNext();
+	}
+	
+	
+	@Override
+	public void getInput(List<Integer>)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
