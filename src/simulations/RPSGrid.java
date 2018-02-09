8706d6cd96d2c5674136615b350cbec0cf002a04
Conrad Mitchell <cmm134@duke.edu>
2018-02-09 14:17:52 -0500

    setting new branch for massive refactor

---
+++ src/simulations/RPSGrid.java
@@ -0,0 +1,16 @@
+package simulations;
+
+public class RPSGrid extends GridModel{
+
+	
+	public RPSGrid()
+	{
+		for(int a=0; a<gridCells.get(0).size(); a++)
+		{
+			for(int b=0;)
+		}
+	}
+	
+	
+	
+}
