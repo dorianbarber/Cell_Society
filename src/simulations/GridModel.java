04111034fd91f17e5fd43d9643f10febf407c2d9
Dorian Barber <dpb20@duke.edu>
2018-02-11 17:36:32 -0500

    DocBuilder

--- src/simulations/GridModel.java
+++ src/simulations/GridModel.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public abstract class GridModel {
 	protected List<List<CellModel>> gridCells;
 	protected int size;
--- src/xml_related_package/XMLBuilder.java
+++ src/xml_related_package/XMLBuilder.java
@@ -1,7 +1,23 @@
 package xml_related_package;
 
+import java.util.List;
 
+import simulations.CellModel;
+import simulations.GridModel;
 
 public class XMLBuilder {
 	
+	
+	
+	
+	public void setUpFile(GridModel model) {
+		
+	}
+	
+	
+	
+	
+	public void writePoints(List<List<CellModel>> grid) {
+		
+	}
 }
